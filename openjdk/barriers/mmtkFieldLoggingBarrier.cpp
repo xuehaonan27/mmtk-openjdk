4d421c6d5a24f1db175ecd42bbe51a24d30976fd
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-10-26 16:05:07 +1100

    Disable clone barrier

--- openjdk/barriers/mmtkFieldLoggingBarrier.cpp
+++ openjdk/barriers/mmtkFieldLoggingBarrier.cpp
@@ -8,7 +8,7 @@ void MMTkFieldLoggingBarrierSetRuntime::record_modified_node_slow(void* src, voi
 }
 
 void MMTkFieldLoggingBarrierSetRuntime::record_clone_slow(void* src, void* dst, size_t size) {
-  ::mmtk_object_reference_clone((MMTk_Mutator) &Thread::current()->third_party_heap_mutator, src, dst, size);
+  // ::mmtk_object_reference_clone((MMTk_Mutator) &Thread::current()->third_party_heap_mutator, src, dst, size);
 }
 
 void MMTkFieldLoggingBarrierSetRuntime::record_modified_node(oop src, ptrdiff_t offset, oop val) {
@@ -26,7 +26,7 @@ void MMTkFieldLoggingBarrierSetRuntime::record_modified_node(oop src, ptrdiff_t 
 }
 
 void MMTkFieldLoggingBarrierSetRuntime::record_clone(oop src, oop dst, size_t size) {
-  record_clone_slow((void*) src, (void*) dst, size);
+  // record_clone_slow((void*) src, (void*) dst, size);
 }
 
 void MMTkFieldLoggingBarrierSetRuntime::record_arraycopy(arrayOop src_obj, size_t src_offset_in_bytes, oop* src_raw, arrayOop dst_obj, size_t dst_offset_in_bytes, oop* dst_raw, size_t length) {
